91a16f8e0b14b5afe47bf697901a13c3b3365436
Nursultan <nursultan.sharsheev@gmail.com>
2020-01-10 21:04:26 +0600

    update

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('.owl-carousel').owlCarousel({
+    $('#photos').owlCarousel({
         loop: true,
         margin: 10,
         nav: true,
@@ -17,6 +17,29 @@ $(document).ready(function () {
             },
             1000: {
                 items: 5
+            }
+        }
+    })
+});
+$(document).ready(function () {
+    $('#videos').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        navText: ["<i class=\"fas fa-arrow-left\"></i>", "<i class=\"fas fa-arrow-right\"></i>"],
+        dots: false,
+        autoplay:true,
+        autoplaySpeed:1000,
+        autoplayTimeout:1000000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
             }
         }
     })
