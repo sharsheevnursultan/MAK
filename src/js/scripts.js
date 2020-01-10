f2ea14fe62fe32b709b6c582aa1289bd8dcc5444
Nursultan <nursultan.sharsheev@gmail.com>
2020-01-10 20:22:50 +0600

    update

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,17 +1,19 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        responsive:{
-            0:{
-                items:1
+        loop: true,
+        margin: 10,
+        nav: true,
+        navText: ["<i class=\"fas fa-arrow-left\"></i>", "<i class=\"fas fa-arrow-right\"></i>"],
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:3
+            600: {
+                items: 3
             },
-            1000:{
-                items:5
+            1000: {
+                items: 5
             }
         }
     })
