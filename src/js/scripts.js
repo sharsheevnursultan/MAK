3ae99a086c46ac02d99e63a8e6423529cff69c04
Sharsheev Nursultan <nursultan.sharsheev@gmail.com>
2020-01-14 17:53:11 +0600

    updates

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -45,3 +45,17 @@ $(document).ready(function () {
     })
 });
 
+let btn = document.querySelector('.topbutton');
+//
+function magic() {
+    if (window.pageYOffset > 200) {
+        btn.style.opacity = '1'
+    } else { btn.style.opacity = '0' }
+}
+//
+btn.onclick = function () {
+    window.scrollTo(0,0)
+};
+
+// When scrolling, we run the function
+window.onscroll = magic;
